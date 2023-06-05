25fa47050013e77d6631fa10f0e35a6a77bfdc2b
Maurice Haghighi Pajouh <mauricehaghighi@gmx.de>
2023-06-05 16:27:32 +0200

    GUI changes
    Conflicts:
    	src/main/java/com/mycompany/password/gen/Encoder.java

--- src/main/java/hawhamburg/pm2_praktikum4/App.java
+++ src/main/java/hawhamburg/pm2_praktikum4/App.java
@@ -3,6 +3,9 @@ package hawhamburg.pm2_praktikum4;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,13 +17,29 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        var label = new Label();
+        BorderPane borderpane = new BorderPane(label);
+        
+        
+        
+         //UNTERNEHMEN;STRASSE;PLZ;ORT;GUETERVERKEHR;PERSONENVERKEHR
+        TableView tableView = new TableView();
+        
+        borderpane.setCenter(tableView);
+        
+        TableColumn<String,String> column1 = new TableColumn<>("test");
+        
+        var scene = new Scene(borderpane,640,480);
+        
+        
         stage.setScene(scene);
+        
         stage.show();
+       
+        
+        
     }
 
   
---
+++ src/main/java/hawhamburg/pm2_praktikum4/Reader.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hawhamburg.pm2_praktikum4;
+
+/**
+ *
+ * @author momo
+ */
+public class Reader {
+    
+    public static final String splitSymbol=";";
+    
+    public void read(String File){
+        
+    }
+}
