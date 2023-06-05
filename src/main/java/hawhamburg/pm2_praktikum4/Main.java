fb747869fca0a9a08044b5fc3c3be10664d03fa3
Maurice Haghighi Pajouh <mauricehaghighi@gmx.de>
2023-06-05 15:21:36 +0200

    init
    Conflicts:
    	src/main/java/com/mycompany/password/gen/Encoder.java

---
+++ src/main/java/hawhamburg/pm2_praktikum4/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hawhamburg.pm2_praktikum4;
+
+/**
+ *
+ * @author momo
+ */
+public class Main {
+    
+    
+   public static void main(String[] args) {
+
+        App.launch(App.class,args);
+
+    }
+    
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module hawhamburg.pm2_praktikum4 {
+    requires javafx.controls;
+    exports hawhamburg.pm2_praktikum4;
+}
