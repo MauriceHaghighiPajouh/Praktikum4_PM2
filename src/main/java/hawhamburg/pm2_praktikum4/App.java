61897c611567b076b58c4d5683cc6cc984659eaa
Maurice Haghighi Pajouh <94959873+MauriceHaghighiPajouh@users.noreply.github.com>
2023-06-06 14:47:08 +0200

    entry class

--- src/main/java/hawhamburg/pm2_praktikum4/App.java
+++ src/main/java/hawhamburg/pm2_praktikum4/App.java
@@ -3,6 +3,7 @@ package hawhamburg.pm2_praktikum4;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -13,6 +14,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -23,24 +25,28 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
 
-        var label = new Label();
-        BorderPane borderpane = new BorderPane(label);
+        Label label = new Label("Sortieren nach: ");
+        BorderPane borderpane = new BorderPane();
+        CheckBox checkbox = new CheckBox("Groß- / Kleinschreibung beachten");
+        label.setFont(new Font("Arial",18));
+        
+
 
         HBox hbox = new HBox();
 
         ComboBox combobox = new ComboBox();
         TextField textfield = new TextField();
 
+        hbox.getChildren().add(label);
         hbox.getChildren().add(combobox);
         hbox.getChildren().add(textfield);
+        hbox.getChildren().add(checkbox);
+        
 
         //UNTERNEHMEN;STRASSE;PLZ;ORT;GUETERVERKEHR;PERSONENVERKEHR
-        TableView tableView = new TableView();
+        TableView<Entry> tableView = new TableView<>();
 
-        
-       
-        
-        
+        tableView.setRowFactory(null);
         
         borderpane.setCenter(tableView);
         borderpane.setTop(hbox);
